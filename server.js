29c19a5babeb9bc25c24d0cd929a7e9fb2ca3d7b
Askar Karasaev <askar.karasaev@gmail.com>
2020-01-02 18:57:53 +0900

    Adding morgan as a logger

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 // Route files
-const bootcamps = require('./routes/bootcamps');
+const bootcamps = require("./routes/bootcamps");
 
 // Load env vars
 dotenv.config({
-	path: './config/config.env'
-})
+  path: "./config/config.env"
+});
 
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
 });
