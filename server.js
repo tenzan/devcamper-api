c1cd070d745b63022662846811b86fb0aacd163e
Askar Karasaev <askar.karasaev@gmail.com>
2020-01-04 22:31:14 +0900

    Colors in the console

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require('colors');
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({
   path: "./config/config.env"
 });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -24,6 +29,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   );
 });
